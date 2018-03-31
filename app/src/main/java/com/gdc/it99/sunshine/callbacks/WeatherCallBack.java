143547b4f63f47e743196e706cf69e1233394319
Caoyaping <1012957507@qq.com>
2018-03-31 11:01:27 +0700

    second commit

---
+++ app/src/main/java/com/gdc/it99/sunshine/callbacks/WeatherCallBack.java
@@ -0,0 +1,11 @@
+package com.gdc.it99.sunshine.callbacks;
+
+/**
+ * Created by IT99 on 2018/3/23.
+ */
+
+public interface WeatherCallBack {
+    interface  LifeAdvice {
+        void lifeAdvice(String index, String advice);
+    }
+}
